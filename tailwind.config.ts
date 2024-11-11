caa326b7615b72be836ee3c21b39f4911d19ae7e
Oleksandr Holyshevskyi <44750553+o-holyshevskyi@users.noreply.github.com>
2024-11-11 11:57:10 +0100

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      keyframes: {
+        floatUp: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-100vh)' },
+        },
+        shake: {
+          '0%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(5px)' },
+          '75%': { transform: 'translateX(-5px)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        float: 'floatUp 5s linear forwards',
+        shake: 'shake 0.5s ease-in-out',
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
